685981609a6ebe9899f3085722ff4082d5e3d3d6
lilxyzw <lilxyzw@gmail.com>
2024-09-25 23:45:38 +0900

    chore: show extension in one column and hide gui on rename

--- Editor/ProjectExtension/ProjectExtension.cs
+++ Editor/ProjectExtension/ProjectExtension.cs
@@ -16,6 +16,8 @@ namespace jp.lilxyzw.editortoolbox
         internal static readonly string[] names = types.Select(t => t.FullName).ToArray();
 
         private static List<IProjectExtensionComponent> projectExtensionComponents;
+        private static string guidEditing = "";
+        private static Dictionary<EditorWindow, (SerializedObject so, SerializedProperty m_IsRenaming, SerializedProperty m_IsWaitingForDelay)> windows = new();
 
         internal static IProjectExtensionComponent Resolve()
         {
@@ -27,19 +29,47 @@ namespace jp.lilxyzw.editortoolbox
         [InitializeOnLoadMethod]
         private static void Init()
         {
+            EditorApplication.update -= EditorUpdate;
+            EditorApplication.update += EditorUpdate;
             EditorApplication.projectWindowItemOnGUI -= Draw;
             EditorApplication.projectWindowItemOnGUI += Draw;
+        }
+
+        private static void EditorUpdate()
+        {
+            guidEditing = "";
+            try
+            {
+                if(EditorWindow.focusedWindow && EditorWindow.focusedWindow.GetType().FullName == "UnityEditor.ProjectBrowser")
+                {
+                    if(!windows.TryGetValue(EditorWindow.focusedWindow, out var values))
+                    {
+                        values.so = new SerializedObject(EditorWindow.focusedWindow);
+                        values.m_IsRenaming = values.so.FindProperty("m_ListAreaState.m_RenameOverlay.m_IsRenaming");
+                        values.m_IsWaitingForDelay = values.so.FindProperty("m_ListAreaState.m_RenameOverlay.m_IsWaitingForDelay");
+                        windows.Add(EditorWindow.focusedWindow, values);
+                    }
+                    values.so.Update();
+                    if(values.m_IsRenaming.boolValue && !values.m_IsWaitingForDelay.boolValue) guidEditing = Selection.assetGUIDs[0];
+                }
+            }
+            catch{}
         }
 
         private static void Draw(string guid, Rect position)
         {
             var path = AssetDatabase.GUIDToAssetPath(guid);
-            if(position.height > 16 || string.IsNullOrEmpty(path)) return;
+            if(position.height > 16 || string.IsNullOrEmpty(path) || guidEditing == guid) return;
 
             var name = Path.GetFileNameWithoutExtension(path);
             var extension = Path.GetExtension(path);
             var currentRect = position;
             currentRect.xMin += EditorStyles.label.CalcSize(new GUIContent(name)).x + 17;
+            if(position.x > 16) // One Column
+            {
+                currentRect.xMin -= 2;
+                currentRect.y -= 1;
+            }
 
             if(projectExtensionComponents == null) Resolve();
 

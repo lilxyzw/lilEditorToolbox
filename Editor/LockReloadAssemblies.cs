b9b839a0e74794a32b2f20df65af82c3cb2fe047
lilxyzw <lilxyzw@gmail.com>
2024-09-29 22:55:28 +0900

    feat: LockReloadAssemblies

---
+++ Editor/LockReloadAssemblies.cs
@@ -0,0 +1,18 @@
+using UnityEditor;
+
+namespace jp.lilxyzw.editortoolbox
+{
+    internal static class LockReloadAssemblies
+    {
+        private const string MENU_PATH = "Tools/lilEditorToolbox/Lock Reload Assemblies";
+        private static bool isLocked = false;
+        [MenuItem(MENU_PATH)]
+        private static void Lock()
+        {
+            isLocked = !isLocked;
+            Menu.SetChecked(MENU_PATH, isLocked);
+            if(isLocked) EditorApplication.LockReloadAssemblies();
+            else EditorApplication.UnlockReloadAssemblies();
+        }
+    }
+}

d28258ffe8e5cec7c85ae77ce26654106e1e3f47
lilxyzw <lilxyzw@gmail.com>
2024-10-02 00:14:50 +0900

    feat: show import window button

--- Editor/ImportExtension/UnitypackageImporter.cs
+++ Editor/ImportExtension/UnitypackageImporter.cs
@@ -3,11 +3,27 @@ using System.Collections;
 using System.Linq;
 using System.Reflection;
 using UnityEditor;
+using UnityEngine.UIElements;
 
 namespace jp.lilxyzw.editortoolbox
 {
     internal class UnitypackageImporter
     {
+        private static readonly MethodInfo MI_HasOpenInstances = typeof(EditorWindow).GetMethod("HasOpenInstances", BindingFlags.Static | BindingFlags.Public);
+
+        private static readonly Type T_ProjectBrowser = typeof(Editor).Assembly.GetType("UnityEditor.ProjectBrowser");
+        private static readonly FieldInfo FI_s_LastInteractedProjectBrowser = T_ProjectBrowser.GetField("s_LastInteractedProjectBrowser", BindingFlags.Static | BindingFlags.Public);
+        private static readonly FieldInfo FI_m_LastFolders = T_ProjectBrowser.GetField("m_LastFolders", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private static readonly Type T_PackageImport = typeof(Editor).Assembly.GetType("UnityEditor.PackageImport");
+        private static readonly MethodInfo MI_HasOpenInstancesPackageImport = MI_HasOpenInstances.MakeGenericMethod(T_PackageImport);
+        private static readonly FieldInfo FI_m_ImportPackageItems = T_PackageImport.GetField("m_ImportPackageItems", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly FieldInfo FI_m_PackageName = T_PackageImport.GetField("m_PackageName", BindingFlags.NonPublic | BindingFlags.Instance);
+        private static readonly MethodInfo MI_ShowTreeGUI = T_PackageImport.GetMethod("ShowTreeGUI", BindingFlags.NonPublic | BindingFlags.Instance);
+
+        private static string folder;
+        private static bool needToReplace = false;
+
         [InitializeOnLoadMethod]
         internal static void Init()
         {
@@ -15,44 +31,51 @@ namespace jp.lilxyzw.editortoolbox
             if(EditorToolboxSettings.instance.unitypackageToFolder) AssetDatabase.importPackageStarted += OnImportPackageStarted;
         }
 
-        private static string folder;
-
         internal static void OnImportPackageStarted(string _)
         {
             if(DragAndDrop.paths.Length == 0) return;
             folder = ProjectWindowPath();
-            if(string.IsNullOrEmpty(folder) || !folder.StartsWith("Assets/")) return;
-            CoroutineHandler.StartStaticCoroutine(ClosePackageImportWindow());
+            needToReplace = !string.IsNullOrEmpty(folder) && folder.StartsWith("Assets/");
+            runtime.CoroutineHandler.StartStaticCoroutine(ClosePackageImportWindow());
         }
 
         private static string ProjectWindowPath()
         {
-            var type = typeof(Editor).Assembly.GetType("UnityEditor.ProjectBrowser");
-            var window = type.GetField("s_LastInteractedProjectBrowser", BindingFlags.Static | BindingFlags.Public).GetValue(null);
-            var folders = type.GetField("m_LastFolders", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(window) as string[];
+            var folders = FI_m_LastFolders.GetValue(FI_s_LastInteractedProjectBrowser.GetValue(null)) as string[];
             return folders.Length == 1 ? folders[0] : "Assets/";
         }
 
         private static IEnumerator ClosePackageImportWindow()
         {
-            var type = typeof(Editor).Assembly.GetType("UnityEditor.PackageImport");
-            var method = typeof(EditorWindow).GetMethod("HasOpenInstances", BindingFlags.Static | BindingFlags.Public);
-            if(method != null)
+            while(!(bool)MI_HasOpenInstancesPackageImport.Invoke(null,null)) yield return null;
+            var window = EditorWindow.GetWindow(T_PackageImport);
+            var m_ImportPackageItems = FI_m_ImportPackageItems.GetValue(window) as object[];
+            var items = m_ImportPackageItems.Select(o => new ImportPackageItemWrap(o)).ToArray();
+
+            // パスの置換
+            if(needToReplace)
             {
-                var genmethod = method.MakeGenericMethod(type);
-                while(!(bool)genmethod.Invoke(null,null))
-                {
-                    yield return null;
-                }
-                var window = EditorWindow.GetWindow(type);
-                var items = (type.GetField("m_ImportPackageItems", BindingFlags.NonPublic | BindingFlags.Instance).GetValue(window) as object[]).Select(o => new ImportPackageItemWrap(o)).ToArray();
                 foreach(var item in items)
                 {
                     if(!string.IsNullOrEmpty(item.existingAssetPath) || !item.destinationAssetPath.StartsWith("Assets/")) continue;
                     item.destinationAssetPath = folder + item.destinationAssetPath.Substring("Assets".Length);
                 }
-                var FI_m_PackageName = type.GetField("m_PackageName", BindingFlags.NonPublic | BindingFlags.Instance);
                 FI_m_PackageName.SetValue(window, $"{FI_m_PackageName.GetValue(window) as string} to \"{folder}\"");
+            }
+
+            // 既存アセットの場所を表示
+            var ShowTreeGUI = (bool)MI_ShowTreeGUI.Invoke(window, new object[]{m_ImportPackageItems});
+            if(!ShowTreeGUI)
+            {
+                var button = new Button(){text = "Show Import Window"};
+                button.style.marginTop = 36;
+                button.style.width = 150;
+                button.clicked += () => {
+                    foreach(var i in items)
+                        if(!i.isFolder) i.assetChanged = true;
+                    window.rootVisualElement.Remove(button);
+                };
+                window.rootVisualElement.Add(button);
             }
         }
     }
@@ -62,12 +85,20 @@ namespace jp.lilxyzw.editortoolbox
         private static readonly Type TYPE = typeof(Editor).Assembly.GetType("UnityEditor.ImportPackageItem");
         private static readonly FieldInfo FI_existingAssetPath = TYPE.GetField("existingAssetPath", BindingFlags.Public | BindingFlags.Instance);
         private static readonly FieldInfo FI_destinationAssetPath = TYPE.GetField("destinationAssetPath", BindingFlags.Public | BindingFlags.Instance);
+        private static readonly FieldInfo FI_isFolder = TYPE.GetField("isFolder", BindingFlags.Public | BindingFlags.Instance);
+        private static readonly FieldInfo FI_assetChanged = TYPE.GetField("assetChanged", BindingFlags.Public | BindingFlags.Instance);
         private object instance;
         public string existingAssetPath => FI_existingAssetPath.GetValue(instance) as string;
         public string destinationAssetPath
         {
             get => FI_destinationAssetPath.GetValue(instance) as string;
             set => FI_destinationAssetPath.SetValue(instance, value);
+        }
+        public bool isFolder => (bool)FI_isFolder.GetValue(instance);
+        public bool assetChanged
+        {
+            get => (bool)FI_assetChanged.GetValue(instance);
+            set => FI_assetChanged.SetValue(instance, value);
         }
 
         public ImportPackageItemWrap(object i) => instance = i;
--- Runtime/CoroutineHandler.cs
+++ Runtime/CoroutineHandler.cs
@@ -1,12 +1,12 @@
 using System.Collections;
 using UnityEngine;
 
-namespace jp.lilxyzw.editortoolbox
+namespace jp.lilxyzw.editortoolbox.runtime
 {
     internal class CoroutineHandler : MonoBehaviour
     {
         private static CoroutineHandler m_Instance;
-        private static CoroutineHandler Instance => m_Instance ? m_Instance : m_Instance = new GameObject("CoroutineHandler"){hideFlags = HideFlags.HideAndDontSave}.AddComponent<CoroutineHandler>();
+        private static CoroutineHandler Instance => m_Instance && m_Instance ? m_Instance : m_Instance = new GameObject("CoroutineHandler"){hideFlags = HideFlags.HideAndDontSave}.AddComponent<CoroutineHandler>();
         private void OnDisable(){if(m_Instance) Destroy(m_Instance.gameObject);}
         internal static Coroutine StartStaticCoroutine(IEnumerator coroutine) => Instance.StartCoroutine(coroutine);
     }
